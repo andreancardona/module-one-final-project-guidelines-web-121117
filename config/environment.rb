3b6f4b7a255a2260f3891cc4923ff248f6cea731
krandles <krandles@gmail.com>
2018-01-02 16:43:51 -0500

    merge test

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 require 'bundler'
 Bundler.require
 require 'rest-client'
+require 'json'
+require 'nokogiri'
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
