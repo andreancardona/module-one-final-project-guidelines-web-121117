c41a25f55dd1716bdf69042d478c5f2d1ecd0969
krandles <krandles@gmail.com>
2018-01-03 10:55:33 -0500

    added genre assignment to book creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,29 @@
 # get book goodreads page link
 # author_hash.css("book").first.css("link").first.text
 
+@unwanted_shelves = ["audiobooks", "novel", "novels", "1001-books", "nonfiction", "paulo-coelho", "to-read", "currently-reading", "sci-fi", "scifi", "favorites", "owned", "sf", "kindle", "audiobook", "default"]
+def find_best_genre(book, bad_shelves = @unwanted_shelves)
+  # book = RestClient.get("#{book_instance.goodreads_url}?format=xml&key=8dQUUUZ8wokkPMjjn2oRxA")
+  # book_hash = Nokogiri::XML(book) #converts to XML format 
+  shelves_array = []
+  book.css("shelf").first(5).each do |shelf_css|
+    shelves_array << shelf_css.first[1]
+  end
+  correct_shelf = shelves_array.find do |shelf|
+    !bad_shelves.include?(shelf)
+  end
+  assign_genre_to_book(correct_shelf)
+end
+
+def assign_genre_to_book(shelf)
+  if Genre.find_by name: shelf
+    genre = Genre.find_by name: shelf
+  else
+    genre = Genre.create(name: shelf)
+  end
+  genre
+end
+
 def author_books_pages_count(books_count)
   bc = books_count.to_i
   if bc <= 30
@@ -29,8 +52,8 @@ def create_book_from_url(url, author_instance)
   description = book_hash.css("description").first.text
   average_rating = book_hash.css("average_rating").first.text
   ratings_count = book_hash.css("ratings_count").first.text
-  # genre to be added here
-  Book.create(title: title, author: author_instance,  published_date: published_date, goodreads_id: goodreads_id, goodreads_url: goodreads_url, publisher: publisher, isbn: isbn, page_count: page_count, average_rating: average_rating, ratings_count: ratings_count, description: description)
+  genre = find_best_genre(book_hash)
+  Book.create(title: title, genre: genre, author: author_instance,  published_date: published_date, goodreads_id: goodreads_id, goodreads_url: goodreads_url, publisher: publisher, isbn: isbn, page_count: page_count, average_rating: average_rating, ratings_count: ratings_count, description: description)
 end
 
 author_id_array = [545, 566, 8132662, 1221698, 1214964, 6466154, 3389, 3565, 88506]
@@ -40,23 +63,24 @@ author_id_array.each do |author_id|
   author = RestClient.get("https://www.goodreads.com/author/show/#{author_id}?format=xml&key=8dQUUUZ8wokkPMjjn2oRxA")
   author_hash = Nokogiri::XML(author)
   author_name = author_hash.css("name").first.text
-  author_instance = Author.create(name: author_name)
+  author_goodreads_id = author_hash.css("id").first.text
+  author_goodreads_url = author_hash.css("link").first.text
+  author_instance = Author.create(name: author_name, goodreads_id: author_goodreads_id, goodreads_url: author_goodreads_url)
 
   author_books_pages = author_books_pages_count(author_hash.css("works_count").first.text)
-  i = 1
+  i = 0
   books_hash = {}
   books_urls_array = []
-  author_books_pages.times do 
+  author_books_pages.times do
+    i += 1
     books = RestClient.get("https://www.goodreads.com/author/list/#{author_id}?format=xml&page=#{i}&key=8dQUUUZ8wokkPMjjn2oRxA")
     books_hash = Nokogiri::XML(books)
     books_hash.css("book").each do |book|
-      # binding.pry
       books_urls_array << book.children.css("link").first.text
     end
     books_urls_array.each do |url|
       create_book_from_url(url, author_instance)
     end
-    i = 0
     books_urls_array = []
   end
 end
