b0bedf56ad387216491456295b856b0858f69797
krandles <krandles@gmail.com>
2018-01-02 20:20:18 -0500

    commit before merge

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,6 @@
 
 author_id_array = [545, 566, 8132662, 1221698, 1214964, 6466154, 3389, 3565, 88506]
 
-
 author_id_array.each do |author_id|
   #binding.pry
   author = RestClient.get("https://www.goodreads.com/author/list/#{author_id}?format=xml&key=8dQUUUZ8wokkPMjjn2oRxA")
